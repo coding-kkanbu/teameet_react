32b6ae02520f27507cdb6fadc5e9e1115f444110
행복한엄쌤 <sendmethere@naver.com>
2023-01-02 20:06:31 +0900

    (1.2) react

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import teameet_logo from '../assets/teameet_logo.svg'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="py-4">
+      <div class="container flex flex-wrap items-center mx-auto">
+        <img src={teameet_logo} alt="logo"></img>
+        <div className='px-8'>모임</div>
+        <div className='px-8'>후기</div>
+        <div className='px-8'>소식</div>
+        <div className='px-8'>홍보관</div>
+      </div>
+    </nav>
   )
 }
